533ad715ea942c2fc98017d7cd6099b4474aa204
ahpu9158 <titiporn02468@gmail.com>
2026-06-04 22:17:00 +0700

    add config to handle google drive pics (ขอแอดเพิ่มตรงนี้หน่อย)

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -5,6 +5,16 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "drive.google.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
         hostname: "images.unsplash.com",
         pathname: "/**",
       },
