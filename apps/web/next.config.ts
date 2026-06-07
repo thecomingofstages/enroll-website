50cf06e706e7be77cdeee5712b951ccd35434f1c
kkattmos <khattiya.mos@gmail.com>
2026-06-07 15:23:26 +0700

    edited next.config.ts for deployment 2

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
+  turbopack: {
       root: "../..",
-    },
   },
   images: {
     remotePatterns: [
