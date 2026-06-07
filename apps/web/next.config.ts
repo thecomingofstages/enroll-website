939b2d2666040ac617a62526a28ea2cfbb6bf7dd
kkattmos <khattiya.mos@gmail.com>
2026-06-07 15:27:25 +0700

    edited next.config.ts for deployment 3

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   turbopack: {
-      root: "../..",
+    root: path.resolve(__dirname, "../.."),
   },
+  outputFileTracingRoot: path.resolve(__dirname, "../.."),
   images: {
     remotePatterns: [
       {
