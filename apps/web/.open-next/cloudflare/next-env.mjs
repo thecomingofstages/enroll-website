fa6d605609a5fc6813e57b8621db34445752b14b
kkattmos <khattiya.mos@gmail.com>
2026-06-07 20:10:10 +0700

    modified package.json 4

---
+++ apps/web/.open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_API_URL":"http://localhost:3001/v1","PROMPTPAY_NUMBER":"0994757766","NEXT_PUBLIC_DIRECTIONS_FALLBACK":"google"};
+export const development = {"NEXT_PUBLIC_API_URL":"http://localhost:3001/v1","PROMPTPAY_NUMBER":"0994757766","NEXT_PUBLIC_DIRECTIONS_FALLBACK":"google"};
+export const test = {"NEXT_PUBLIC_API_URL":"http://localhost:3001/v1","PROMPTPAY_NUMBER":"0994757766","NEXT_PUBLIC_DIRECTIONS_FALLBACK":"google"};
